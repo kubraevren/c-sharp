271229ee13ad85ce90fca997987b7affde43cfe7
Kübra Evren <19010310050@ogrenci.bartin.edu.tr>
2021-12-07 15:52:52 +0300

    kareHesaplama

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi, kare;
+            Console.Write("Sayıyı giriniz:");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            kare = sayi * sayi;
+            Console.Write("Sayının karesi: " + kare);
+            Console.Read();
+
+        }
+    }
+}
