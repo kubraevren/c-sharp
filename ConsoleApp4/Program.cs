bbab76899fe59758be748dcf61e909218b94f70f
Kübra Evren <19010310050@ogrenci.bartin.edu.tr>
2021-12-07 16:07:12 +0300

    Toplamaİslemi

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1, s2;
+            Console.Write("1. sayıyı giriniz:");
+            s1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("2. sayıyı giriniz.");
+            s2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("sayıların toplamı: " + (s1 + s2));
+            Console.ReadLine();
+        }
+    }
+}
