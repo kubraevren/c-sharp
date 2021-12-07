ccfd239cda770216cbb3651b5bac8ce8b2e27bd9
Kübra Evren <19010310050@ogrenci.bartin.edu.tr>
2021-12-07 15:41:27 +0300

    veriAlmak

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ad;
+            Console.Write("Adınızı yazınız:");
+            ad = Console.ReadLine();
+            Console.WriteLine("Merhaba! " + ad);
+            Console.ReadLine();
+
+            
+        }
+    }
+}
