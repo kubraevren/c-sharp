d8fbd4f4718c710bed6e50687f0cfea2507ac5eb
Kübra Evren <19010310050@ogrenci.bartin.edu.tr>
2021-12-07 16:33:02 +0300

    Alan ve Çevre Hesaplama

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Uzun kenarı giriniz:");
+            int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Kısa kenarı giriniz:");
+            int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Dikdörtgenin çevresi : " + (sayi1 + sayi2) * 2);
+            Console.WriteLine("Dikdörtgenin alanı : " + (sayi1 * sayi2));
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
