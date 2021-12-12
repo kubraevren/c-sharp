ab2969a687159893e015d24d99a6598b24dbfc55
Kübra Evren <19010310050@ogrenci.bartin.edu.tr>
2021-12-12 13:18:33 +0300

    goto ile sadece pozitif sayıların karesini almak

---
+++ ax+b/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ax_b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sayiIsteme:
+            Console.Write("sayı giriniz:");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi > 0)
+                Console.WriteLine("karesi:" + sayi * sayi);
+            else
+                goto sayiIsteme;
+
+            Console.ReadLine();
+
+        }
+    }
+}
