6380774d62eeeb0d5025a54b8039436bdf9fea37
Kübra Evren <19010310050@ogrenci.bartin.edu.tr>
2021-12-12 13:39:50 +0300

    1'den n'e kadar sayı yazdırma c#
    1'den n'e kadar sayı yazdırma c#

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,s = 0;
+         
+            Console.WriteLine("n değeri giriniz:");
+            n = Convert.ToInt32(Console.ReadLine());
+            don:
+            s += 1;
+            Console.Write(s+ " ");
+            if (s < n) goto don;
+           
+
+            Console.ReadLine();
+
+        }
+    }
+}
