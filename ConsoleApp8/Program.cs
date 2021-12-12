7ad4e33738659b17b0e86192a118c6db886b9607
Kübra Evren <19010310050@ogrenci.bartin.edu.tr>
2021-12-12 12:38:25 +0300

    inç değerini cm çevirme c#

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double oran = 2.54;
+            Console.Write("inç değerini giriniz:");
+            double inch = Convert.ToDouble(Console.ReadLine());
+
+            double santim = inch * oran;
+            Console.WriteLine("inç değerinin cm karşılığı: " + santim);
+
+            Console.ReadLine();
+
+        }
+    }
+}
