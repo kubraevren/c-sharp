cfd6f38941d3403ab514aaf8f65fe5ee14328809
Kübra Evren <19010310050@ogrenci.bartin.edu.tr>
2021-12-12 14:00:31 +0300

    sayıları sayaçla ekranda tutan uygulama
    konsoldan girilen sayıların sayısını sayaçla tutan,her adımda girilen sayıyı ekrana yazdıran, girilen sayı 5 olduğunda programı bitirip sayaç değerini ekrana yazdıran konsol uygulaması

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0 ; int s = 0;
+            sayiIsteme:
+            Console.Write("n değeri giriniz:");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            s += 1;
+            Console.WriteLine(n);
+
+            if (n != 5)
+                goto sayiIsteme;
+            else
+                Console.Write("sayaç "+ s);
+
+            Console.ReadLine();
+        }
+    }
+}
