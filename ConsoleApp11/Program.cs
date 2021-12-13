a021669d1d72fafec4e92a4c9f28d6ca879012e1
Kübra Evren <19010310050@ogrenci.bartin.edu.tr>
2021-12-13 11:06:20 +0300

    girilen sayıya kadar çift sayıları yazdırma

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n , s = 0;
+            Console.Write("n giriniz:");
+            n = Convert.ToInt32(Console.ReadLine());
+        basla:
+             s += 2;
+                Console.WriteLine(s);
+            if (s < n-1)
+                goto basla;
+            
+            
+            Console.ReadLine();
+        }
+
+    }
+}
