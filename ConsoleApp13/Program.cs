b98f6bf24acfd1d63ffb21e9e86519b35c95923e
Kübra Evren <19010310050@ogrenci.bartin.edu.tr>
2021-12-13 13:55:59 +0300

    1'den n'e kadar sayı toplamı bulma for-while c#

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayac,n,toplam = 0;
+            Console.WriteLine("n değerini giriniz: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+             for (sayac=1;  sayac <= n; sayac++)
+                 toplam = toplam + sayac;
+                 Console.WriteLine("sayaç: "+sayac);
+                 Console.WriteLine("toplam " + toplam); 
+           /* int sayacc = 1;
+            while (sayacc <= n)
+            {
+                toplam = toplam + sayacc;
+                sayacc++;
+            }
+            Console.WriteLine("while ile toplamı " + toplam); */
+
+            Console.ReadLine();
+
+        }
+    }
+}
