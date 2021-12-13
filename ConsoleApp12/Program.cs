7e676d8d9d91d273ef1583409e1255467a2f1446
Kübra Evren <19010310050@ogrenci.bartin.edu.tr>
2021-12-13 13:10:59 +0300

    for-if-while ile merhaba yazmak c#

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Merhaba Dünya");
+            }
+            //****************************************************
+            int k = 1;
+        yaz:
+            Console.WriteLine("Merhaba Dünya");
+            k += 1;
+            if (k <= 5)
+                goto yaz;
+
+            //**************************************************
+
+            int a = 1;
+            while (a <= 10)
+            {
+                Console.WriteLine("Merhaba Dünya");
+                a++;
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
