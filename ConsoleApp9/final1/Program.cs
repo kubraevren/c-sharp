55b195535e7b2ef47eb84a0b51c5ee0ac667b75c
Kübra Evren <19010310050@ogrenci.bartin.edu.tr>
2022-01-07 13:20:08 +0300

    faktoriyelll

---
+++ ConsoleApp9/final1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace final1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // hem for hem while kullanarak faktoriyel hesaplama:
+
+            /* int f = 1; 
+            Console.WriteLine("bir sayı giriniz:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int sayac = 1; sayac <= n; sayac++)
+            {
+                f = f * sayac;
+
+            }
+            Console.WriteLine(f); */
+
+            int f = 1;
+            int sayac = 1;
+            Console.WriteLine("bir sayı giriniz:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            while (sayac <= n)
+            {
+                f *= sayac;
+                sayac++;
+            }
+
+            Console.WriteLine("faktoriyel: " + f);
+
+
+
+        }
+    }
+}
