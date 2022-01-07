8981d900b1e560833d8d55cfe36cdc31a1b64a1e
Kübra Evren <19010310050@ogrenci.bartin.edu.tr>
2022-01-07 14:28:43 +0300

    metotlar

---
+++ final2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace final2
+{
+    class Program
+    // . Ekrana metot kullanarak n defa “Merhaba” çıktısı veren
+    // konsol uygulamasını yazınız. N değeri kullanıcıdan alınmalı
+    // ve parametrik olarak metoda gönderilmelidir.
+    {
+        static void Main(string[] args)
+        {
+            //değiskenleri tanımlayalim
+            int n;
+            //konsoldan n değerini okuyalım
+            Console.Write("n:");
+            n = Convert.ToInt32(Console.ReadLine());
+            //yaz isimli metodu n defa çallstıralım
+            for (int i = 1; i <= n; i++)
+                yaz();
+            Console.Read(); } //bekle 
+                              //yaz isimli parametresiz metot
+        static void yaz()
+        {
+            Console.WriteLine("Merhaba");
+        }
+     
+    }
+}
+
